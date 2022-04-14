76e3f62a2fbc88634c9aeb6e1b48f6baf77162af
Srutip04 <subuhishruti75@gmail.com>
2022-04-14 23:50:56 +0530

    Templating engines

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname,'views'));
+
 const PORT =  3000;
 
 
@@ -22,6 +25,12 @@ app.use((req,res,next) =>{
 app.use('/site',express.static(path.join(__dirname,'public')));
 app.use(express.json());
 
+app.get('/', (req,res) =>{
+  res.render('index',{
+    title: 'My friends are very clever',
+    caption: 'Lets go skiing',
+  })
+})
 app.use('/friends',friendsRouter);
 app.use("/messages", messagesRouter);
 
